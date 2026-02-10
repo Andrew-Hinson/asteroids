2ad642c9b614fd9a7f7c83d333e4573d1271e532
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-02-10 17:04:45 -0600

    clock management

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    game_clock = pygame.time.Clock()    
+    date_time = 0
     while True:
         log_state()
 
@@ -15,6 +16,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        delta_time = game_clock.tick(60)
+        date_time = delta_time / 1000
         
 if __name__ == "__main__":
     main()
