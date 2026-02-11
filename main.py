e361d06da61871eb5039096492d565abc4f77e2c
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-02-10 20:42:13 -0600

    I have a triangle rendered on screen woopee

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()    
     date_time = 0
+
+    player1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
 
@@ -15,9 +20,11 @@ def main():
                 return
 
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         delta_time = game_clock.tick(60)
         date_time = delta_time / 1000
+
         
 if __name__ == "__main__":
     main()
